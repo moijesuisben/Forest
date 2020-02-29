90c22d075071d4623f2fb07b54e8a4427bcae0a7
moijesuisben <benji.apg@gmail.com>
2020-02-29 20:02:07 +0100

    Destroy animal when offscreen

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,16 +6,21 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
 
     private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
